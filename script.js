264434006b42f9084c3a0463135ba89283edbe25
tjlearnscode <121098526+tjlearnscode@users.noreply.github.com>
2024-06-30 16:17:35 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 5;
 
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
+//const ___  new Image creates a new image element and holds it in a variable.
+// it has the same functionality as document.createElement("img")
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'layer-1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'layer-2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'layer-3.png';
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'layer-4.png';
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'layer-5.png';
 
-const playerImg = new Image;
-playerImg.src = "shadow_dog.png";/*image is 6876px wide containing 12cols
-and 5230px in height with 10rows*/
 
+class Layer {
+    constructor(image, speedMod) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedMod = speedMod;
+        this.speed = gameSpeed * this.speedMod;
+    }
+    update(){
+        this.speed = gameSpeed * this.speedMod;
+        if(this.x <= -this.width) {
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if (this.x2 <= -this.width) {
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Match.floor(this.x2 - this.speed);
+    }
+    draw(){
 
-const SPRITE_WIDTH = 573; //sprite sheet width/12(cols)
-const SPRITE_HEIGHT = 523; //sprite sheet height/10rows
-let frameX = 1;
-let frameY = Number(document.getElementById('sprite-row').value);
-const PLAYER_W = 50;
-const PLAYER_H = 50;
-let maxCol;
+    }
+}
 
 function animate(){
-    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    //ctx.fillRect(100,50,100,100);
-    //ctx.drawImage(image,sx,sy,sw,sh,dx,dy,dw,dh)
-    ctx.drawImage(playerImg,frameX * SPRITE_WIDTH,frameY * SPRITE_HEIGHT,SPRITE_WIDTH,SPRITE_HEIGHT,0,0,PLAYER_W,PLAYER_H);
-    switch (frameY) {
-        case 0:
-        case 1:
-        case 2:
-        case 6:
-        case 7: {
-            if (frameX % 6 == 0) {
-                frameX = 0
-            } else {
-                frameX++
-            };
-        };
-            break;
-        case 3: {
-            if (frameX / 9 == 1) {
-                frameX = 0;
-            } else {
-                frameX++;
-            }
-        };
-            break;
-    }
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT)
     requestAnimationFrame(animate);
-    };
+}
 
-animate();
+animate()
