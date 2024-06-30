5cd226fb2793e66a7c9b81d3400dcda891e0dcb3
tjlearnscode <121098526+tjlearnscode@users.noreply.github.com>
2024-06-30 16:15:42 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImg = new Image;
+playerImg.src = "shadow_dog.png";/*image is 6876px wide containing 12cols
+and 5230px in height with 10rows*/
+
+
+const SPRITE_WIDTH = 573; //sprite sheet width/12(cols)
+const SPRITE_HEIGHT = 523; //sprite sheet height/10rows
+let frameX = 1;
+let frameY = Number(document.getElementById('sprite-row').value);
+const PLAYER_W = 50;
+const PLAYER_H = 50;
+let maxCol;
+
+function animate(){
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    //ctx.fillRect(100,50,100,100);
+    //ctx.drawImage(image,sx,sy,sw,sh,dx,dy,dw,dh)
+    ctx.drawImage(playerImg,frameX * SPRITE_WIDTH,frameY * SPRITE_HEIGHT,SPRITE_WIDTH,SPRITE_HEIGHT,0,0,PLAYER_W,PLAYER_H);
+    switch (frameY) {
+        case 0:
+        case 1:
+        case 2:
+        case 6:
+        case 7: {
+            if (frameX % 6 == 0) {
+                frameX = 0
+            } else {
+                frameX++
+            };
+        };
+            break;
+        case 3: {
+            if (frameX / 9 == 1) {
+                frameX = 0;
+            } else {
+                frameX++;
+            }
+        };
+            break;
+    }
+    requestAnimationFrame(animate);
+    };
+
+animate();
