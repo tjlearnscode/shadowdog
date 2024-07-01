e37bb4a161ef5a4faccd936f0f6b5d6c4e3222c0
tjlearnscode <121098526+tjlearnscode@users.noreply.github.com>
2024-06-30 22:22:07 -0400

    slowed down animation frames

--- script.js
+++ script.js
@@ -15,67 +15,72 @@ let frameX = 1;
 let frameY = 6;
 const PLAYER_W = 50;
 const PLAYER_H = 50;
-let maxCol;
+const STAGGER_FRAMES = 5;
+let gameFrame = 0;
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
     //ctx.fillRect(100,50,100,100);
     //ctx.drawImage(image,sx,sy,sw,sh,dx,dy,dw,dh)
     ctx.drawImage(playerImg,frameX * SPRITE_WIDTH,frameY * SPRITE_HEIGHT,SPRITE_WIDTH,SPRITE_HEIGHT,0,0,PLAYER_W,PLAYER_H);
-    switch (frameY) {
-        case 0:
-        case 1:
-        case 2:
-        case 6:
-        case 7: {
-            if (frameX / 6 == 1) {
-                frameX = 0
-            } else {
+    if (gameFrame % STAGGER_FRAMES == 0) {
+        switch (frameY) {
+            case 0:
+            case 1:
+            case 2:
+            case 6:
+            case 7: {
+                if (frameX / 6 == 1) {
+                    frameX = 0
+                } else {
                 frameX++
+                };
             };
+                break;
+            case 3: {
+                if (frameX / 8 == 1) {
+                    frameX = 0;
+                } else {
+                    frameX++;
+                }
+            };
+                break;
+            case 4: {
+                if (frameX / 10 == 1) {
+                    frameX = 0;
+                } else {
+                    frameX++;
+                }
+            };
+                break;
+            case 5: {
+                if (frameX / 4 == 1) {
+                    frameX = 0;
+                } else {
+                    frameX++;
+                }
+            };
+                break;
+            case 8: {
+                if (frameX / 11 == 1) {
+                    frameX = 0;
+                } else {
+                    frameX++;
+                }
+            };
+                break;
+            case 10: {
+                if (frameX / 3 == 1) {
+                    frameX = 0;
+                } else {
+                    frameX++;
+                }
+            };
+                break;
         };
-            break;
-        case 3: {
-            if (frameX / 8 == 1) {
-                frameX = 0;
-            } else {
-                frameX++;
-            }
         };
-            break;
-        case 4: {
-            if (frameX / 10 == 1) {
-                frameX = 0;
-            } else {
-                frameX++;
-            }
-        };
-            break;
-        case 5: {
-            if (frameX / 4 == 1) {
-                frameX = 0;
-            } else {
-                frameX++;
-            }
-        };
-            break;
-        case 8: {
-            if (frameX / 11 == 1) {
-                frameX = 0;
-            } else {
-                frameX++;
-            }
-        };
-            break;
-        case 10: {
-            if (frameX / 3 == 1) {
-                frameX = 0;
-            } else {
-                frameX++;
-            }
-        };
-            break;
-    }
+        gameFrame++;
+
     requestAnimationFrame(animate);
     };
 
